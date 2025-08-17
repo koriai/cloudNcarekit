f08cf27f4f14d0fc6de344b7c1a3f6ea772a41f5
KIM HYUN JIN <hyunjin@koriai.com>
2025-08-17 12:50:25 +0900

    Initial Commit

---
+++ cloudNcarekit/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  cloudNcarekit
+//
+//  Created by Ken on 8/17/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
